8a77e0ba5936e6a396889779a747a322f17920de
Rabiul Hasan Rafee <rhr277@gmail.com>
2025-05-05 17:06:38 +0600

    service page added

---
+++ src/pages/ServicePage/Service.jsx
@@ -0,0 +1,11 @@
+import { Banner } from "../../components/ServicePageComponents/Banner";
+import { TransSolution } from "../../components/ServicePageComponents/TransSolution";
+
+export const Service = () => {
+  return (
+    <div className="container mx-auto mt-20">
+      <Banner />
+      <TransSolution />
+    </div>
+  );
+};
