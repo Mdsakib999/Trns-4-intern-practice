c82f5f5e16709ab14e5934b433f756230cd71a0a
Rabiul Hasan Rafee <rhr277@gmail.com>
2025-05-05 18:32:53 +0600

    service page done

--- src/components/ServicePageComponents/TransSolution.jsx
+++ src/components/ServicePageComponents/TransSolution.jsx
@@ -3,12 +3,15 @@ import { Tab } from "./Tab/Tab";
 
 export const TransSolution = () => {
   return (
-    <div className="mt-28">
-      <h1 className="text-6xl font-semibold text-center">
-        Transport <span className="text-primary">Solutions</span>
-      </h1>
-      <Tab />
-      <CustomClearance />
+    <div className="mt-28 mx-10">
+      <div className="relative">
+        <h1 className="text-6xl font-semibold text-center">
+          Transport <span className="text-primary">Solutions</span>
+        </h1>
+        <div className="absolute -inset-x-20 -inset-y-20 bg-gradient-to-r from-blue-600 via-indigo-800 to-purple-900 opacity-50 blur-2xl h-[300px] w-[500px] rounded-2xl"></div>
+        <Tab />
+        <CustomClearance />
+      </div>
     </div>
   );
 };
