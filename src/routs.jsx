9c8b40b461a8814ff9171a93809b31e327cb573e
Md. Shahidul Islam <shanto57575@gmail.com>
2025-05-06 12:05:50 +0600

    added about page with resposnvieness

--- src/routs.jsx
+++ src/routs.jsx
@@ -2,22 +2,27 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import { Service } from "./pages/ServicePage/Service";
 import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/services",
-        element: <Service />,
-      },
-    ],
-  },
+	{
+		path: "/",
+		element: <App />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/about",
+				element: <About />,
+			},
+			{
+				path: "/services",
+				element: <Service />,
+			},
+		],
+	},
 ]);
 
 export default router;
