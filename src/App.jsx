036f4d713e211068cbf4fc6770983736b58ef3fd
Rabiul Hasan Rafee <rhr277@gmail.com>
2025-05-06 15:13:28 +0600

    responsive fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Footer } from "./components/shared/Footer";
 
 const App = () => {
   return (
-    <div className="font-inter">
+    <div className="font-inter max-w-7xl mx-auto">
       <Navbar />
       <div className="pt-0 min-h-screen">
         <Outlet />
--- src/components/ServicePageComponents/TransSolution.jsx
+++ src/components/ServicePageComponents/TransSolution.jsx
@@ -4,7 +4,7 @@ import { Tab } from "./Tab/Tab";
 export const TransSolution = () => {
   return (
     <div className="mt-28 px-4 md:px-10 max-w-7xl mx-auto relative">
-      <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold text-center z-10 relative">
+      <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold text-center z-0 relative">
         Transport <span className="text-primary">Solutions</span>
       </h1>
 
@@ -12,7 +12,7 @@ export const TransSolution = () => {
       <div className="absolute -top-10 left-1/2 transform -translate-x-1/2 w-[90%] sm:w-[500px] h-[300px] rounded-2xl blur-3xl opacity-40 bg-gradient-to-r from-blue-600 via-indigo-800 to-purple-900 z-0" />
 
       {/* Tab section and Custom Clearance */}
-      <div className="relative z-10 mt-10">
+      <div className="relative z-0 mt-10">
         <Tab />
         <CustomClearance />
       </div>
