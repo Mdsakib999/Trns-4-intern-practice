3e35d2780be850bcda401d8ec0cea1da8d9e6640
Md. Shahidul Islam <shanto57575@gmail.com>
2025-05-07 17:00:57 +0600

    new animation added

--- src/components/HomePageComponents/Banner.jsx
+++ src/components/HomePageComponents/Banner.jsx
@@ -4,16 +4,24 @@ const Banner = () => {
 	return (
 		<div className="relative w-full">
 			{/* Banner Image */}
-			<img src={bannerImage} alt="Banner" className="w-full object-cover" />
+			<img
+				data-aos="zoom-in"
+				src={bannerImage}
+				alt="Banner"
+				className="w-full object-cover"
+			/>
 
 			{/* Dark overlay only for small devices */}
 			<div className="absolute inset-0 bg-black/40 lg:bg-transparent" />
 
 			{/* Text over image */}
-			<div className="absolute top-24 left-1/2 transform -translate-x-1/2 text-center w-full px-4 md:top-32 lg:top-48">
-				<h1 className="text-xl md:text-5xl font-semibold font-inter drop-shadow-md leading-5 md:leading-12 text-white lg:text-black">
-					<p>Flexible Logistics &</p>
-					<p>Cargo Services</p>
+			<div
+				data-aos="fade-up"
+				className="absolute top-24 left-1/2 transform -translate-x-1/2 text-center w-full px-4 md:top-32 lg:top-48"
+			>
+				<h1 className="text-xl max-w-md mx-auto md:text-5xl font-semibold font-inter drop-shadow-md leading-5 md:leading-12 text-white lg:text-black">
+					<span>Flexible Logistics & </span>
+					<span>Cargo Services</span>
 				</h1>
 			</div>
 		</div>
--- src/components/ServicePageComponents/Banner.jsx
+++ src/components/ServicePageComponents/Banner.jsx
@@ -3,56 +3,59 @@ import parcel from "/assets/parcelSVG.png";
 import airplane from "/assets/airplaneSVG.png";
 
 export const Banner = () => {
-  const cardItems = [
-    {
-      id: 1,
-      name: "CARRYING THE LOAD",
-      desc: "Free Download Cargo Crate SVG vector file in monocolor and multicolor type for Sketch and Figma from Cargo Crate Vectors svg vector collection. Cargo Crate Vectors SVG vector illustration.",
-      image: ship,
-    },
-    {
-      id: 2,
-      name: "CARRYING THE LOAD",
-      desc: "Free Download Cargo Crate SVG vector file in monocolor and multicolor type for Sketch and Figma from Cargo Crate Vectors svg vector collection. Cargo Crate Vectors SVG vector illustration.",
-      image: parcel,
-    },
-    {
-      id: 3,
-      name: "CARRYING THE LOAD",
-      desc: "Free Download Cargo Crate SVG vector file in monocolor and multicolor type for Sketch and Figma from Cargo Crate Vectors svg vector collection. Cargo Crate Vectors SVG vector illustration.",
-      image: airplane,
-    },
-  ];
+	const cardItems = [
+		{
+			id: 1,
+			name: "CARRYING THE LOAD",
+			desc: "Free Download Cargo Crate SVG vector file in monocolor and multicolor type for Sketch and Figma from Cargo Crate Vectors svg vector collection. Cargo Crate Vectors SVG vector illustration.",
+			image: ship,
+		},
+		{
+			id: 2,
+			name: "CARRYING THE LOAD",
+			desc: "Free Download Cargo Crate SVG vector file in monocolor and multicolor type for Sketch and Figma from Cargo Crate Vectors svg vector collection. Cargo Crate Vectors SVG vector illustration.",
+			image: parcel,
+		},
+		{
+			id: 3,
+			name: "CARRYING THE LOAD",
+			desc: "Free Download Cargo Crate SVG vector file in monocolor and multicolor type for Sketch and Figma from Cargo Crate Vectors svg vector collection. Cargo Crate Vectors SVG vector illustration.",
+			image: airplane,
+		},
+	];
 
-  return (
-    <div className="px-4 md:px-10 max-w-7xl mx-auto pt-3">
-      <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold text-center md:text-left mx-auto md:mx-0 mt-10">
-        <p>Learn More About the</p>
-        <p>
-          Segments <span className="text-primary">We Serve</span>
-        </p>
-      </h1>
+	return (
+		<div className="px-4 md:px-10 max-w-7xl mx-auto pt-3">
+			<h1 className="w-full mx-auto md:mx-0 text-center md:text-left text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold mt-10">
+				<span className="block">Learn More About the</span>
+				<span className="block text-primary font-bold">
+					Segments <span className="text-primary">We Serve</span>
+				</span>
+			</h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mt-12 justify-items-center my-10">
-        {cardItems.map((cardItem) => (
-          <div
-            key={cardItem.id}
-            className="w-full max-w-xs rounded-lg p-6 text-center hover:bg-secondary transition"
-          >
-            <div className="bg-primary rounded-md p-3 inline-block mb-4">
-              <img
-                src={cardItem.image}
-                alt={cardItem.name}
-                className="w-20 h-20 object-contain"
-              />
-            </div>
-            <h2 className="text-primary text-lg font-bold mb-2">
-              {cardItem.name}
-            </h2>
-            <p className="text-xs text-primary leading-snug">{cardItem.desc}</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+			<div className="grid grid-cols-1 md:grid-cols-3 gap-10 mt-12 justify-items-center my-10">
+				{cardItems.map((cardItem) => (
+					<div
+						key={cardItem.id}
+						className="w-full max-w-xs rounded-lg p-6 text-center hover:bg-secondary transition"
+					>
+						<div
+							data-aos="zoom-in"
+							className="bg-primary rounded-md p-3 inline-block mb-4"
+						>
+							<img
+								src={cardItem.image}
+								alt={cardItem.name}
+								className="w-20 h-20 object-contain"
+							/>
+						</div>
+						<h2 className="text-primary text-lg font-bold mb-2">
+							{cardItem.name}
+						</h2>
+						<p className="text-xs text-primary leading-snug">{cardItem.desc}</p>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
